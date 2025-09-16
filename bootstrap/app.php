15fa80a5e9c010ef4cc643c9d2764755be5d9292
Milan <76396495+themilan1337@users.noreply.github.com>
2025-09-16 14:35:26 +0500

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\HandleInertiaRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,13 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            HandleInertiaRequests::class,
-        ]);
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
     })
-
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
